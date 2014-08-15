0e8c592c0421bf405bce9b6c8484f62ed1a0ef1d
Brian Ballsun-Stanton <brian@fedarch.org>
2014-08-15 15:21:37 +1000

    trench unit

--- getMax.sql
+++ getMax.sql
@@ -1,5 +1,17 @@
 select uuid, aenttypename, freetext from latestnondeletedarchentidentifiers;
-select max(cast(freetext as integer)) from latestnondeletedaentvalue join attributekey using (attributeid) where attributename = 'Unit ID' and uuid in (
+
+
+
+select uuid, a.response from identifierasspreadsheet a join latestnondeletedaentvalue using (uuid) join attributekey using (attributeid) 
+where attributename = 'Unit ID' and uuid in (
 select child.uuid from latestnondeletedaentreln parent join latestnondeletedaentreln child using (relationshipid)
 where parent.uuid != child.uuid
-and parent.uuid = 1000011408066189727);
+);
+
+
+
+
+
+select childuuid, trench.response || ' - ' || unit.response
+from (select uuid, response from identifierasspreadsheet join latestnondeletedarchent using (uuid) join aenttype using (aenttypeid) where aenttypename = 'Trench') trench join latestnondeletedaentreln parent using (uuid) join latestnondeletedaentreln child using (relationshipid) join 
+(select uuid as childuuid, response from identifierasspreadsheet join latestnondeletedarchent using (uuid) join aenttype using (aenttypeid) where aenttypename = 'Unit') unit on (child.uuid = childuuid);
